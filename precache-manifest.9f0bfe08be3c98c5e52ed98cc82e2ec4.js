2275dc125c2af85af33b0617e1f82c14319c8fed
Rasan <rashmedo@gmail.com>
2019-03-28 22:53:56 -0400

    Updates

---
+++ precache-manifest.9f0bfe08be3c98c5e52ed98cc82e2ec4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "550c79de082ae6f7d9ff",
+    "url": "/clickgame/static/js/runtime~main.5bc58040.js"
+  },
+  {
+    "revision": "61de685978a38a3cf98d",
+    "url": "/clickgame/static/js/main.23cd0f3e.chunk.js"
+  },
+  {
+    "revision": "973eca460e14e0ddebe0",
+    "url": "/clickgame/static/js/2.693cc7d6.chunk.js"
+  },
+  {
+    "revision": "61de685978a38a3cf98d",
+    "url": "/clickgame/static/css/main.bd165e5b.chunk.css"
+  },
+  {
+    "revision": "6ad141c81b5bf6aa4aac33eadd8abf12",
+    "url": "/clickgame/index.html"
+  }
+];
